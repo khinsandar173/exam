931919bbe09bff111c177a3c4ba4542c4c3fa06c
khin173 <khinsandarsoe.codex@gmail.com>
2020-10-15 17:18:57 +0630

    exam

---
+++ test2/exe11.php
@@ -0,0 +1,20 @@
+<?php
+	echo '
+		<table border="1" cellspacing="0px">
+			<tr >
+				<td>Salary of Mr A . is </td>
+				<td>1000$</td>
+			</tr>
+			<tr >
+				<td>Salary of Mr B . is </td>
+				<td>1000$</td>
+			</tr>
+			<tr>
+				<td>Salary of Mr C . is </td>
+				<td>1000$</td>
+			</tr>
+
+
+	';
+	echo '</table>'
+?>
